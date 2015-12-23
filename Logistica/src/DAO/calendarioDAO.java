17dfad8d71a286c874afed269b71952429258483
jeanpaulo <jeanpaulo@jean.Quality.br>
2015-12-23 09:23:35 -0200

    SHOW

--- Logistica/src/DAO/calendarioDAO.java
+++ Logistica/src/DAO/calendarioDAO.java
@@ -13,50 +13,69 @@ import dominio.calendario;
 public class calendarioDAO {
 	ConectaBanco conexao = new ConectaBanco();
 	Statement stm;
-	String msg ;
 	ResultSet rs;
 	ArrayList<calendario> calendarios = new ArrayList<calendario>();
+	String msg;
 	
-	public ArrayList<calendario> obterCalendarioPorColetor(String coletor){
-		conexao.conexao();
-		
-		try{
-		stm = conexao.conn.createStatement();
-		String sql = "select  pr.numero_proposta as PROPOSTA, pr.empresa as EMPRESA, am.numero_amostra as AMOSTRA,"
-				+ " am.periodicidade as PERIODICIDADE, aos.ordem as ORDEM,  pa.descricao as PARAMETRO, fr.descricao as FRASCO, vol.volume as VOLUME,"
-				+ " un.unidade_medida as UNIDADEMEDIDA, pre.descricao as PRESERVACAO, aos.datacoleta as DATACOLETA, aos.coletor as COLETOR"
-				+ " from proposta as pr, amostra as am, amostra_os as aos, parametro as pa, frasco as fr, volume as vol,"
-				+ " unidade_medida as un, preservacao as pre, coletor as co, amostra_parametro as ap"
-				+ " where aos.coletor='"+coletor+"' and pr.idproposta = aos.proposta and aos.proposta = ap.proposta"
-				+ " and aos.amostra = am.idamostra and aos.amostra =ap.amostra and pa.idparametro = ap.parametro"
-				+ " and fr.id_frasco = pa.frasco and vol.id_volume = pa.volume and pre.id_preservacao = pa.preservacao"
-				+ " and un.id_unidade_medida = vol.id_unidade_medida and aos.coletor = co.nome";
-		
-		rs = stm.executeQuery(sql);
-		
-		while(rs.next()){
-		
-		calendario c = new calendario(rs.getString("PROPOSTA"), rs.getString("EMPRESA"),rs.getString("AMOSTRA"),
-		rs.getString("PERIODICIDADE"),rs.getInt("ORDEM"),rs.getString("PARAMETRO"),rs.getString("FRASCO"),
-		rs.getDouble("VOLUME"),rs.getString("UNIDADEMEDIDA"),rs.getString("PRESERVACAO"),rs.getString("DATACOLETA"),
-		rs.getString("COLETOR"));
-		
-		calendarios.add(c);
-		
-		}
-		
-		}
-		
-		catch(SQLException ex){
-			JOptionPane.showMessageDialog(null, "ERRO"+ex.getMessage());
-			
-		}finally{
-		conexao.desconecta();
-		
-		}
-		
-		return calendarios;
-		
-	}
+	public void PreencherTabela(String sql, ArrayList dados) {
 
+		conexao.conexao();
+		conexao.executaSQL(sql);
+		try {
+
+			if (conexao.rs.first()) {
+				dados.clear();
+				do {
+					dados.add(new Object[] {
+
+							conexao.rs.getObject("PROPOSTA"), conexao.rs.getObject("EMPRESA"), conexao.rs.getObject("AMOSTRA"),
+							conexao.rs.getObject("PERIODICIDADE"), conexao.rs.getObject("ORDEM"), conexao.rs.getObject("PARAMETRO"),
+							 conexao.rs.getObject("FRASCO"), conexao.rs.getObject("VOLUME"), conexao.rs.getObject("UNIDADEMEDIDA"),
+							 conexao.rs.getObject("PRESERVACAO"), conexao.rs.getObject("DATACOLETA"), conexao.rs.getObject("COLETOR"),
+							
+					});
+
+				} while (conexao.rs.next());
+			}else{
+				dados.clear();
+			}
+
+		} catch (SQLException e) {
+			JOptionPane.showMessageDialog(null, e.getMessage());
+
+		} finally {
+			conexao.desconecta();
+	}
+	}
+	
+	
+	public void PreencherTabelaSoma(String sql, ArrayList dados) {
+
+		conexao.conexao();
+		conexao.executaSQL(sql);
+		try {
+
+			if (conexao.rs.first()) {
+				dados.clear();
+				do {
+					dados.add(new Object[] {
+
+							conexao.rs.getObject("FRASCO"), conexao.rs.getObject("PRESERVACAO"), conexao.rs.getObject("SOMA"),
+							conexao.rs.getObject("UN")
+							
+					});
+
+				} while (conexao.rs.next());
+			}else{
+				dados.clear();
+			}
+
+		} catch (SQLException e) {
+			JOptionPane.showMessageDialog(null, e.getMessage());
+
+		} finally {
+			conexao.desconecta();
+	}
+	}
+	
 }
--- Logistica/src/face/TelaCalendario.java
+++ Logistica/src/face/TelaCalendario.java
@@ -1,11 +1,13 @@
 package face;
 
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 import javax.swing.JButton;
@@ -18,24 +20,30 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSeparator;
 import javax.swing.JTable;
-import javax.swing.JTextArea;
+import javax.swing.ListSelectionModel;
 import javax.swing.SwingConstants;
 import javax.swing.border.TitledBorder;
+import javax.swing.table.DefaultTableCellRenderer;
 
+import utilitarios.ModeloTable;
 import DAO.amostraDAO;
 import DAO.calendarioDAO;
 
 import com.toedter.calendar.JDateChooser;
-
-import dominio.calendario;
+import javax.swing.JTextField;
 
 public class TelaCalendario extends JFrame {
 
 	private JPanel contentPane;
-	private ArrayList dados;
 	private JTable TableCalendario;
-	private JScrollPane scrollCalendario;
 	private calendarioDAO calendario = new calendarioDAO();
+	private ArrayList dados2;
+	private String[] colunas;
+	private String sql;
+	private ArrayList dadosSoma;
+	private String[] colunasSoma;
+	private JTable TableCalendarioSoma;
+	
 	
 	public TelaCalendario() {
 		setTitle("Calend\u00E1rio");
@@ -46,6 +54,23 @@ public class TelaCalendario extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
+		TableCalendario = new JTable();
+		TableCalendario.setRequestFocusEnabled(false);
+		
+		TableCalendarioSoma  = new JTable();
+		TableCalendarioSoma.setRequestFocusEnabled(false);
+		
+		dados2 = new ArrayList();
+		colunas = new String[] { "PROPOSTA", "EMPRESA","AMOSTRA","PERIODICIDADE", "ORDEM", "PARAMETRO", "FRASCO", "VOLUME","UN", "PRESERVACAO","DATACOLETA","COLETOR" };
+
+		ModeloTable modelo = new ModeloTable(dados2, colunas);
+		TableCalendario.setModel(modelo);
+		
+		dadosSoma = new ArrayList();
+		colunasSoma = new String[] { "FRASCO","PRESERVACAO", "SOMA","UN"};
+		
+		ModeloTable modeloSoma = new ModeloTable(dadosSoma, colunasSoma);
+		TableCalendarioSoma.setModel(modeloSoma);
 		
 		final JDateChooser dateChooser = new JDateChooser();
 		dateChooser.setFont(new Font("Tahoma", Font.PLAIN, 13));
@@ -58,15 +83,20 @@ public class TelaCalendario extends JFrame {
 		lblData.setBounds(10, 60, 83, 20);
 		contentPane.add(lblData);
 		
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(10, 135, 1062, 517);
-		contentPane.add(scrollPane);
+		JScrollPane scrollCalendario = new JScrollPane();
+		scrollCalendario.setBounds(10, 114, 1062, 375);
+		contentPane.add(scrollCalendario);
 		
+		JLabel lblObservaes = new JLabel("Observa\u00E7\u00F5es:");
+		lblObservaes.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblObservaes.setBounds(20, 500, 108, 20);
+		contentPane.add(lblObservaes);
 		
-		final JTextArea txtarea = new JTextArea();
-		txtarea.setBounds(10, 135, 1062, 517);
-		txtarea.setLineWrap(true);
-		scrollPane.setViewportView(txtarea);
+		JScrollPane scrollCalendarioSoma = new JScrollPane();
+		scrollCalendarioSoma.setBounds(10, 541, 1062, 110);
+		contentPane.add(scrollCalendarioSoma);
+
+		
 		
 		final JCheckBox checkOnOff = new JCheckBox("Ativar Alertas");
 		checkOnOff.setHorizontalAlignment(SwingConstants.CENTER);
@@ -106,78 +136,6 @@ public class TelaCalendario extends JFrame {
 		cbcoletor.setBounds(83, 31, 108, 20);
 		contentPane.add(cbcoletor);
 		
-		JButton btnPesquisar = new JButton("Pesquisar");
-		btnPesquisar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-					
-				
-	
-				
-				if(!cbcoletor.getSelectedItem().equals(" ") && dateChooser.getDate()== null){
-						//APENAS COLETOR
-						
-					ArrayList<calendario> calendarios = new ArrayList<calendario>();
-					
-					calendarios = calendario.obterCalendarioPorColetor(cbcoletor.getSelectedItem().toString());
-				
-		for (int i = 0; i <= calendarios.size() - 1; i++){
-		System.out.println("PROPOSTA: "+calendarios.get(i).getProposta()+" | EMPRESA: "+calendarios.get(i).getEmpresa()+""
-		+ " | AMOSTRA: "+calendarios.get(i).getAmostra()+" | PERIODICIDADE: "+calendarios.get(i).getPeriodicidade()+" "
-		 + "| ORDEM DE COLETA: "+calendarios.get(i).getOrdem()+"º COLETA | DATA DA COLETA: "+calendarios.get(i).getDatacoleta()+" "
-		 + "| COLETOR: "+calendarios.get(i).getColetor());
-		
-		
-		System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------------------------");
-		}						
-					
-					
-				
-						
-						
-					}else 
-				if(!cbcoletor.getSelectedItem().equals(" ") && dateChooser.getDate() != null){
-					// COLETOR E DATA
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					}
-				
-				
-				else if(cbcoletor.getSelectedItem().equals(" ") && dateChooser.getDate() != null){
-						//FILTRAR POR DATA
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-						
-						}
-				else{
-						JOptionPane.showMessageDialog(null, "escolha um filtro");
-					}
-			}
-			} );
-		
-		btnPesquisar.setBounds(203, 30, 141, 23);
-		contentPane.add(btnPesquisar);
-		
-		JButton btnAmostrasNoDatadas = new JButton("Amostras N\u00E3o Datadas");
-		btnAmostrasNoDatadas.setBounds(20, 94, 171, 23);
-		contentPane.add(btnAmostrasNoDatadas);
-		
 		JButton btnGerarPdf = new JButton("Gerar PDF");
 		btnGerarPdf.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -185,8 +143,6 @@ public class TelaCalendario extends JFrame {
 		});
 		btnGerarPdf.setBounds(201, 61, 141, 23);
 		contentPane.add(btnGerarPdf);
-		
-	
 		
 		lblDesativado.setVisible(true);
 		lblAtivado.setVisible(false);
@@ -204,11 +160,191 @@ public class TelaCalendario extends JFrame {
 			}
 		});
 			amostraDAO dao = new amostraDAO();
-			ArrayList<String> dados = dao.obterColetores();
+			final ArrayList<String> dados = dao.obterColetores();
 		
 			for (int i = 0; i <= dados.size() - 1; i++)
 			cbcoletor.addItem(dados.get(i));
+			
+		
+		JButton btnPesquisar = new JButton("Pesquisar");
+		btnPesquisar.setBounds(203, 30, 141, 23);
+		contentPane.add(btnPesquisar);
+		btnPesquisar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				
+				
+				
+	
+				
+				if(!cbcoletor.getSelectedItem().equals(" ") && dateChooser.getDate()== null){
+						//APENAS COLETOR
+				
+					sql = "select  pr.numero_proposta as PROPOSTA, pr.empresa as EMPRESA, am.numero_amostra as AMOSTRA,"
+							+ " am.periodicidade as PERIODICIDADE, aos.ordem as ORDEM,  pa.descricao as PARAMETRO, fr.descricao as FRASCO, vol.volume as VOLUME,"
+							+ " un.unidade_medida as UNIDADEMEDIDA, pre.descricao as PRESERVACAO, aos.datacoleta as DATACOLETA, aos.coletor as COLETOR"
+							+ " from proposta as pr, amostra as am, amostra_os as aos, parametro as pa, frasco as fr, volume as vol,"
+							+ " unidade_medida as un, preservacao as pre, coletor as co, amostra_parametro as ap"
+							+ " where aos.coletor='"+cbcoletor.getSelectedItem().toString()+"' and pr.idproposta = aos.proposta and aos.proposta = ap.proposta"
+							+ " and aos.amostra = am.idamostra and aos.amostra =ap.amostra and pa.idparametro = ap.parametro"
+							+ " and fr.id_frasco = pa.frasco and vol.id_volume = pa.volume and pre.id_preservacao = pa.preservacao"
+							+ " and un.id_unidade_medida = vol.id_unidade_medida and aos.coletor = co.nome";
+					
+					calendario.PreencherTabela(sql, dados2);
+				
+					TableCalendario.setAutoCreateRowSorter(true);
+					
+					}else 
+				if(!cbcoletor.getSelectedItem().equals(" ") && dateChooser.getDate() != null){
+					// COLETOR E DATA
+					
+					String data = new SimpleDateFormat("dd/MM/yyyy").format(dateChooser.getDate());
+					
+					sql = "select  pr.numero_proposta as PROPOSTA, pr.empresa as EMPRESA, am.numero_amostra as AMOSTRA,"
+							+ " am.periodicidade as PERIODICIDADE, aos.ordem as ORDEM,  pa.descricao as PARAMETRO, fr.descricao as FRASCO, vol.volume as VOLUME,"
+							+ " un.unidade_medida as UNIDADEMEDIDA, pre.descricao as PRESERVACAO, aos.datacoleta as DATACOLETA, aos.coletor as COLETOR"
+							+ " from proposta as pr, amostra as am, amostra_os as aos, parametro as pa, frasco as fr, volume as vol, unidade_medida as un, preservacao as pre, coletor as co, amostra_parametro as ap"
+							+ " where aos.coletor = '"+cbcoletor.getSelectedItem().toString()+"' AND aos.datacoleta = '"+data+"' and pr.idproposta = aos.proposta and aos.proposta = ap.proposta and aos.amostra = am.idamostra "
+							+ " and aos.amostra =ap.amostra and pa.idparametro = ap.parametro and fr.id_frasco = pa.frasco "
+							+ " and vol.id_volume = pa.volume and pre.id_preservacao = pa.preservacao and un.id_unidade_medida = vol.id_unidade_medida and aos.coletor = co.nome";
+					
+					calendario.PreencherTabela(sql, dados2);
+				
+					TableCalendario.setAutoCreateRowSorter(true);
+				
+					
+					
+					
+				String	sqlSoma = "SELECT fr.descricao as frasco, pre.descricao as preservacao, SUM(vol.volume) as soma , un.unidade_medida as un"
+						+ " FROM proposta as pr, amostra as am, amostra_os as aos, parametro as pa, amostra_parametro as ap, frasco as fr, volume as vol, preservacao as pre,"
+						+ " unidade_medida as un, coletor as co WHERE aos.datacoleta = '"+data+"' and  aos.coletor = '"+cbcoletor.getSelectedItem().toString()+"'"
+						+ " and pr.idproposta = aos.proposta and aos.proposta = ap.proposta and aos.amostra = am.idamostra and aos.amostra =ap.amostra and"
+						+ " pa.idparametro = ap.parametro and fr.id_frasco = pa.frasco and vol.id_volume = pa.volume and pre.id_preservacao = pa.preservacao and "
+						+ " un.id_unidade_medida = vol.id_unidade_medida and aos.coletor = co.nome group by fr.descricao, vol.volume, pre.descricao, un.unidade_medida";
+				
+				
+				calendario.PreencherTabelaSoma(sqlSoma, dadosSoma);
+				
+				TableCalendarioSoma.setAutoCreateRowSorter(true);
+			
+					
+					}
+				
+				
+				else if(cbcoletor.getSelectedItem().equals(" ") && dateChooser.getDate() != null){
+						//FILTRAR POR DATA
+					
+					String data = new SimpleDateFormat("dd/MM/yyyy").format(dateChooser.getDate());
+
+					sql =   "select  pr.numero_proposta as PROPOSTA, pr.empresa as EMPRESA, am.numero_amostra as AMOSTRA, "
+							+ " am.periodicidade as PERIODICIDADE, aos.ordem as ORDEM,  pa.descricao as PARAMETRO, fr.descricao as FRASCO, vol.volume as VOLUME, "
+							+ " un.unidade_medida as UNIDADEMEDIDA, pre.descricao as PRESERVACAO, aos.datacoleta as DATACOLETA, aos.coletor as COLETOR "
+							+ " from proposta as pr, amostra as am, amostra_os as aos, parametro as pa, amostra_parametro as ap, "
+							+ " frasco as fr, volume as vol, preservacao as pre , unidade_medida as un, coletor as co "
+							+ " where aos.datacoleta = '"+data+"' and pr.idproposta = aos.proposta and aos.proposta = ap.proposta "
+							+ " and aos.amostra = am.idamostra and aos.amostra =ap.amostra and pa.idparametro = ap.parametro "
+							+ " and fr.id_frasco = pa.frasco and vol.id_volume = pa.volume and pre.id_preservacao = pa.preservacao "
+							+ " and un.id_unidade_medida = vol.id_unidade_medida and aos.coletor = co.nome";
+					
+					calendario.PreencherTabela(sql, dados2);
+				
+					TableCalendario.setAutoCreateRowSorter(true);
+					
+					
+					
+						
+						}
+				else{
+						JOptionPane.showMessageDialog(null, "escolha um filtro");
+					}
+			}
+			} );
+		
+		
+		try {
+
+			scrollCalendarioSoma.setViewportView(TableCalendarioSoma);
+
+			TableCalendarioSoma.setSurrendersFocusOnKeystroke(true);
+			TableCalendarioSoma.setFocusTraversalPolicyProvider(true);
+			TableCalendarioSoma.setFocusCycleRoot(true);
+			TableCalendarioSoma.setForeground(new Color(0, 0, 0));
+			TableCalendarioSoma.setSelectionForeground(new Color(0, 0, 0));
+			TableCalendarioSoma.setFillsViewportHeight(true);
+			TableCalendarioSoma.setSelectionBackground(new Color(135, 206, 235));
+			TableCalendarioSoma.setAutoCreateRowSorter(true);
+
+			TableCalendarioSoma.getColumnModel().getColumn(0).setPreferredWidth(400);
+			TableCalendarioSoma.getColumnModel().getColumn(1).setPreferredWidth(400);
+			TableCalendarioSoma.getColumnModel().getColumn(2).setPreferredWidth(50);
+			TableCalendarioSoma.getColumnModel().getColumn(3).setPreferredWidth(50);
+			
+			TableCalendarioSoma.getTableHeader().setReorderingAllowed(false);
+			TableCalendarioSoma.setAutoResizeMode(JTable.AUTO_RESIZE_NEXT_COLUMN);
+			TableCalendarioSoma.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+			
+			TableCalendarioSoma.setDefaultRenderer(Object.class, new DefaultTableCellRenderer() {
+
+				public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected,
+						boolean hasFocus, int row, int column) {
+					super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+					this.setHorizontalAlignment(CENTER);
+					return this;
+				}
+			});
+
+		} catch (Exception ex) {
+			JOptionPane.showMessageDialog(null, "ERRO" + ex.getMessage());
+		} finally {
+			TableCalendarioSoma.requestFocus();
+		}
 
 		
-	}
+		
+		try {
+
+			scrollCalendario.setViewportView(TableCalendario);
+
+			TableCalendario.setSurrendersFocusOnKeystroke(true);
+			TableCalendario.setFocusTraversalPolicyProvider(true);
+			TableCalendario.setFocusCycleRoot(true);
+			TableCalendario.setForeground(new Color(0, 0, 0));
+			TableCalendario.setSelectionForeground(new Color(0, 0, 0));
+			TableCalendario.setFillsViewportHeight(true);
+			TableCalendario.setSelectionBackground(new Color(135, 206, 235));
+			TableCalendario.setAutoCreateRowSorter(true);
+
+			TableCalendario.getColumnModel().getColumn(0).setPreferredWidth(100);
+			TableCalendario.getColumnModel().getColumn(1).setPreferredWidth(100);
+			TableCalendario.getColumnModel().getColumn(2).setPreferredWidth(100);
+			TableCalendario.getColumnModel().getColumn(3).setPreferredWidth(100);
+			TableCalendario.getColumnModel().getColumn(4).setPreferredWidth(100);
+			TableCalendario.getColumnModel().getColumn(5).setPreferredWidth(100);
+			TableCalendario.getColumnModel().getColumn(6).setPreferredWidth(100);
+			TableCalendario.getColumnModel().getColumn(7).setPreferredWidth(100);
+			TableCalendario.getColumnModel().getColumn(8).setPreferredWidth(100);
+			TableCalendario.getColumnModel().getColumn(9).setPreferredWidth(100);
+			TableCalendario.getColumnModel().getColumn(10).setPreferredWidth(100);
+			TableCalendario.getColumnModel().getColumn(11).setPreferredWidth(100);
+			
+			TableCalendario.getTableHeader().setReorderingAllowed(false);
+			TableCalendario.setAutoResizeMode(JTable.AUTO_RESIZE_NEXT_COLUMN);
+			TableCalendario.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+			
+			TableCalendario.setDefaultRenderer(Object.class, new DefaultTableCellRenderer() {
+
+				public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected,
+						boolean hasFocus, int row, int column) {
+					super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+					this.setHorizontalAlignment(CENTER);
+					return this;
+				}
+			});
+
+		} catch (Exception ex) {
+			JOptionPane.showMessageDialog(null, "ERRO" + ex.getMessage());
+		} finally {
+			TableCalendario.requestFocus();
+		}
+}
 }
